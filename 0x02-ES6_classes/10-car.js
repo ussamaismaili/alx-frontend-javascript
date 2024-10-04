9bed9ef3820b3800b2d2576b158164dc1c8b3fff
Oussama ismaili <ismailiossama@gmail.com>
2024-10-04 03:43:15 +0100

    7-10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+export default class Car {
+  constructor(brand = null, motor = null, color = null) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clone = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      clone[symbol] = this[symbol];
+    });
+    return clone;
+  }
+}
---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
